12f35db80319e5682f83e5571522b0b55f62e89e
“prefolo” <webintosh@yahoo.it>
2022-10-16 19:31:11 +0200

    Add function that display the gameboard content

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 Gameboard = (function () {
-	const marks = [new Array(3), new Array(3), new Array(3)];
+	const marks = new Array(9);
 
-	function display() {}
+	marks[0] = 'x';
+	marks[5] = 'o';
+	marks[3] = 'x';
+	marks[8] = 'o';
+
+	function display() {
+		const cells = document.querySelectorAll('.cell');
+
+		for (let i = 0; i < marks.length; i++) {
+			const mark = marks[i];
+
+			if (mark == 'x') cells[i].textContent = 'x';
+
+			if (mark == 'o') cells[i].textContent = 'o';
+		}
+	}
 
 	return { display };
 })();
+
+Gameboard.display();

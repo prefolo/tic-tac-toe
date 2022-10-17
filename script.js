14a6e8f322ec67f14cd25ed4d97778734a8f5c36
“prefolo” <webintosh@yahoo.it>
2022-10-17 09:53:53 +0200

    Add victory check

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const Gameboard = (function () {
-	let marks = new Array(9);
+	let marks = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 	function display() {
 		const cells = document.querySelectorAll('.cell');
@@ -13,7 +13,8 @@ const Gameboard = (function () {
 	}
 
 	function setMarkIntoCell(mark, cell) {
-		if (marks[cell.dataset.index] != undefined) return 0;
+		if (Game.getGameIsOver()) return;
+		if (marks[cell.dataset.index] != 0) return 0;
 
 		if (mark == 'x' || mark == 'o') marks[cell.dataset.index] = mark;
 		display();
@@ -21,11 +22,51 @@ const Gameboard = (function () {
 	}
 
 	function clear() {
-		marks = new Array(9);
+		marks = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 		display();
 	}
 
-	return { setMarkIntoCell, clear };
+	function getWinnerMark() {
+		const parts = [
+			marks.slice(0, 3), // row1
+			marks.slice(3, 6), // row2
+			marks.slice(6), // row3
+			[marks[0], marks[3], marks[6]], // col1
+			[marks[1], marks[4], marks[7]], // col2
+			[marks[2], marks[5], marks[8]], // col3
+			[marks[0], marks[4], marks[8]], // diagonal1
+			[marks[2], marks[4], marks[6]], // diagonal2
+		];
+
+		let winnerMark;
+
+		for (const part of parts) {
+			if (allAreEqual(part)) {
+				winnerMark = part[0];
+				break;
+			}
+		}
+
+		if (winnerMark) return winnerMark;
+		if (winnerMark == undefined && gameboardIsFull()) return 'tie';
+		if (winnerMark == undefined && !gameboardIsFull()) return 0;
+	}
+
+	function gameboardIsFull() {
+		return marks.every((item) => item != 0);
+	}
+
+	function allAreEqual(array) {
+		const result = array.every((item) => {
+			if (item === array[0] && item != 0) {
+				return true;
+			}
+		});
+
+		return result;
+	}
+
+	return { setMarkIntoCell, clear, getWinnerMark };
 })();
 
 const Player = (name, mark) => {
@@ -39,16 +80,22 @@ const Game = (function () {
 	let player1;
 	let player2;
 	let currentPlayer;
+	let gameIsOver;
+
+	const getGameIsOver = () => gameIsOver;
 
 	function start() {
 		Gameboard.clear();
 
+		gameIsOver = false;
 		player1 = Player(1, 'o');
 		player2 = Player(1, 'x');
 		currentPlayer = player1;
 	}
 
 	function markCell(cell) {
+		if (gameIsOver) return;
+
 		const isMarked = Gameboard.setMarkIntoCell(
 			currentPlayer.getMark(),
 			cell
@@ -56,9 +103,24 @@ const Game = (function () {
 
 		if (isMarked)
 			currentPlayer = currentPlayer == player1 ? player2 : player1;
+
+		switch (Gameboard.getWinnerMark()) {
+			case 'o':
+				console.log('Player1 (mark o) won!');
+				gameIsOver = true;
+				break;
+			case 'x':
+				console.log('Player2 (mark x) won!');
+				gameIsOver = true;
+				break;
+			case 'tie':
+				console.log('Tie!');
+				gameIsOver = true;
+				break;
+		}
 	}
 
-	return { start, markCell };
+	return { start, markCell, getGameIsOver };
 })();
 
 document.querySelectorAll('.cell').forEach((cell) =>
